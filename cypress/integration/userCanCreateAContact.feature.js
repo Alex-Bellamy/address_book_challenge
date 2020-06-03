4085f6182bcd92ab7f2b57a5a2ea2e24e51e4880
Alex Bellamy <alexlbellamy@gmail.com>
2020-06-03 16:09:05 +0200

    brackets added after the add contact and submit functions in the test file enabling tests to pass

--- cypress/integration/userCanCreateAContact.feature.js
+++ cypress/integration/userCanCreateAContact.feature.js
@@ -2,14 +2,14 @@ describe('user can create a contact', () => {
     
     it ('test', () => {
         cy.visit('http://localhost:3002')
-        cy.get("#add-contact").click
+        cy.get("#add-contact").click()
         cy.get("#name").type ("Alex")
         cy.get("#email").type ("alexlbellamy@gmail.com")
         cy.get("#phone").type ("0752 111111")
         cy.get("#company").type ("Craft Academy")
         cy.get("#notes").type ("Enjoying the sun")
         cy.get("#twitter").type ("@alexbellamy")
-        cy.get("#submit").click
+        cy.get("#submit").click()
     })
 
     it ('dispays a name of the new contact', () => {
