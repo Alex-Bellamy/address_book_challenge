7a1ae56d51a907fab0961a403dbee713f9ab1b9e
Alex Bellamy <alexlbellamy@gmail.com>
2020-06-04 10:54:55 +0200

    adding toggling visibility function, hot server and styling

--- cypress/integration/userCanCreateAContact.feature.js
+++ cypress/integration/userCanCreateAContact.feature.js
@@ -1,6 +1,6 @@
 describe('user can create a contact', () => {
     
-    it ('test', () => {
+    beforeEach('test', () => {
         cy.visit('http://localhost:3002')
         cy.get("#add-contact").click()
         cy.get("#name").type ("Alex")
